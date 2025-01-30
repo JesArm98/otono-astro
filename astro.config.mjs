851b010faccb2136e2aa4ce97a039ffaeeab9e95
AdrianJacquezDual <dual.innovacion@tuvanosa.com>
2025-01-30 15:50:02 -0700

    contacto mobil hecho

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({
+export default {
   integrations: [react(), tailwind()],
   devToolbar: {
     enabled: false, // Desactiva la Astro Dev Toolbar
   },
-});
+};
